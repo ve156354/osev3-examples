b6baa06176af444bfffe5449c67674c70f17e865
ve156354 <veerendra_akula@yahoo.com>
2016-07-11 14:36:04 -0400

    Update Application.java

--- spring-boot/helloworld-springboot-maven/src/main/java/hello/Application.java
+++ spring-boot/helloworld-springboot-maven/src/main/java/hello/Application.java
@@ -18,13 +18,12 @@ public class Application {
 	@RequestMapping("/")
 	public String home() {
 		
-		return "Hello Docker / Openshift World.";
+		//return "Hello Docker / Openshift World.";
 		
 		
-		//return "Hello Docker / Openshift World !!! \n" + " \n database_db2_dbUrl: " + env.getProperty("database_db2_dbUrl")  
-		//       + " \n database_db2_dbPassword: " + env.getProperty("database_db2_dbPassword")  + " \n database_db2_dbPassword1: " 
-		//	   + env.getProperty("database_db2_dbPassword1") + " \n database_db2_dbPassword2: " + env.getProperty("database_db2_dbPassword2")
-		//	   + " \n db2name: " + env.getProperty("db2name");
+		return "Hello Docker / Openshift World !!! \n" + " \n db2url: " + env.getProperty("db2url")  
+		       + " \n mysqlurl: " + env.getProperty("mysqlurl")  + " \n myconfigkey: " 
+			   + env.getProperty("myconfigkey");
 	}
 
 
