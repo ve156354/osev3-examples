bc6bc5671f0704f76256474172ad7ec45e0f67eb
ve156354 <veerendra_akula@yahoo.com>
2016-07-31 16:43:58 -0400

    Update Application.java

--- spring-boot/helloworld-springboot-maven/src/main/java/hello/Application.java
+++ spring-boot/helloworld-springboot-maven/src/main/java/hello/Application.java
@@ -18,7 +18,7 @@ public class Application {
 	@RequestMapping("/")
 	public String home() {
 		
-		return "Hello Docker / Openshift World from local env....";
+		return "Hello Docker / Openshift World from local env....V1";
 		
 		
 		//return "Hello Docker / Openshift World !!! \n" + " \n db2url: " + env.getProperty("db2url")  
